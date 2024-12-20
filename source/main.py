8e76f5bfaea5551a21404b06514fe1884181fea0
khosta77 <puwerfulpants@gmail.com>
2024-12-20 15:25:07 +0300

    Небольшие перестановки

--- source/main.py
+++ source/main.py
@@ -14,10 +14,6 @@ from torch import device, cuda
 from sklearn.model_selection import train_test_split
 
 from constant import PATH, NUM_CLASSES, MAX_SENTENCE, EPOCHS
-from universalds import UniversalDataset
-from modelbertclassifier import ModelBertClassifier
-from modelalbertclassifier import ModelAlBertClassifier
-from modeldebertaclassifier import ModelDeBertaClassifier
 from trainer import Trainer
 from models import init_models
 
@@ -35,10 +31,11 @@ if __name__ == "__main__":
             train_size=0.9, random_state=42)
     print(f'Обучающая выборка: {len(train_data):,}\nВалидация: {len(valid_data):,}')
 
-    trainer = Trainer(models=models, batch_size=16, epochs=EPOCHS, device=DEVICE)
+    trainer = Trainer(models=models, batch_size=32, epochs=15, device=DEVICE)
 
     result = trainer.train_models(
         train_data['annotation'], train_data['rate'],
-        valid_data['annotation'], valid_data['rate']
+        valid_data['annotation'], valid_data['rate'],
+        accurate_break=0.8
     )
     result.to_csv('result.csv')
