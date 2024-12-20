739c94af2c5f68516c54b6a2c7696d9fa33d12f7
khosta77 <puwerfulpants@gmail.com>
2024-12-20 16:00:42 +0300

    Изменил константы

--- source/constant.py
+++ source/constant.py
@@ -1,5 +1,5 @@
-PATH='./data/nlp_rate_table.csv'
-NUM_CLASSES=11
+PATH='./data/nlp_good_or_not_good_labirint.csv'
+NUM_CLASSES=2
 MAX_SENTENCE=512
-BATCH_SIZE=16
-EPOCHS=20
+BATCH_SIZE=32
+EPOCHS=15
