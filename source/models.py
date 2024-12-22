e54a977548d70ddbfcca33672cd452e375eba421
khosta77 <puwerfulpants@gmail.com>
2024-12-22 12:59:20 +0300

    Докинул еще одну модель, раскомитил две первые модели

--- source/models.py
+++ source/models.py
@@ -1,45 +1,55 @@
 def init_models(num_classes, max_sentence, device):
     return [
+        #{  # По итогу не дала результата
+        #    'model'     : 'distilbert/distilbert-base-uncased',
+        #    'n_classes' : num_classes,
+        #    'max_len'   : max_sentence,
+        #    'device'    : device,
+        #    'tokenizer' : 'distilbert/distilbert-base-uncased',
+        #    'criterion' : 'BCELoss',
+        #    'optimizer' : 'AdamW',
+        #    'name'      : 'DistilBert'
+        #},
+        #{  # Слишком большая....
+        #    'model'     : 'albert/albert-base-v2',
+        #    'n_classes' : num_classes,
+        #    'max_len'   : max_sentence,
+        #    'device'    : device,
+        #    'tokenizer' : 'albert/albert-base-v2',
+        #    'criterion' : 'BCELoss',
+        #    'optimizer' : 'AdamW',
+        #    'name'      : 'AlbertV2'
+        #},
         {
-            'model'     : 'distilbert/distilbert-base-uncased',
+            'model'     : 'google-bert/bert-base-uncased',
             'n_classes' : num_classes,
             'max_len'   : max_sentence,
             'device'    : device,
-            'tokenizer' : 'distilbert/distilbert-base-uncased',
+            'tokenizer' : 'google-bert/bert-base-uncased',
             'criterion' : 'BCELoss',
             'optimizer' : 'AdamW',
-            'name'      : 'DistilBert'
+            'name'      : 'Bert'
         },
         {
-            'model'     : 'albert/albert-base-v2',
+            'model'     : 'DeepPavlov/rubert-base-cased-sentence',
             'n_classes' : num_classes,
             'max_len'   : max_sentence,
             'device'    : device,
-            'tokenizer' : 'albert/albert-base-v2',
+            'tokenizer' : 'DeepPavlov/rubert-base-cased-sentence',
             'criterion' : 'BCELoss',
             'optimizer' : 'AdamW',
-            'name'      : 'AlbertV2'
-        }
-        #{
-        #    'model'     : 'DeepPavlov/rubert-base-cased-sentence',
-        #    'n_classes' : num_classes,
-        #    'max_len'   : max_sentence,
-        #    'device'    : device,
-        #    'tokenizer' : 'DeepPavlov/rubert-base-cased-sentence',
-        #    'criterion' : 'CrossEntropyLoss',
-        #    'optimizer' : 'AdamW',
-        #    'name'      : 'DeepPavlov'
-        #},
-        #{
-        #    'model'     : 'sentence-transformers/LaBSE',
-        #    'n_classes' : num_classes,
-        #    'max_len'   : max_sentence,
-        #    'device'    : device,
-        #    'tokenizer' : 'sentence-transformers/LaBSE',
-        #    'criterion' : 'CrossEntropyLoss',
-        #    'optimizer' : 'AdamW',
-        #    'name'      : 'LaBSE'
-        #},
+            'name'      : 'DeepPavlov'
+        },
+        {
+            'model'     : 'sentence-transformers/LaBSE',
+            'n_classes' : num_classes,
+            'max_len'   : max_sentence,
+            'device'    : device,
+            'tokenizer' : 'sentence-transformers/LaBSE',
+            'criterion' : 'BCELoss',
+            'optimizer' : 'AdamW',
+            'name'      : 'LaBSE'
+        },
         #{  # На RTX 4060 16Gb эту модель запустить нельзя
         #    'model'     : 'albert/albert-xxlarge-v2',
         #    'n_classes' : num_classes,
